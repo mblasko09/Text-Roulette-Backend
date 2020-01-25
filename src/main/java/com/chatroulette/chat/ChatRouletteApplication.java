fd2fdb39b8d741445407794f330204b4c186f52b
Mark Davis Blasko <mblasko@g.clemson.edu>
2020-01-25 15:31:23 -0500

    initial commit

---
+++ src/main/java/com/chatroulette/chat/ChatRouletteApplication.java
@@ -0,0 +1,13 @@
+package com.chatroulette.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRouletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRouletteApplication.class, args);
+	}
+
+}
